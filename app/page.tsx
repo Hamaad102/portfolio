f28549dd7a16d8857a99a6cfa1090aaffdc2f3db
Hamaad Chughtai <Hamaad102@gmail.com>
2024-12-12 00:28:12 -0500

    package - bumped version to 0.3.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
 
 import { motion } from 'motion/react'
 
@@ -21,24 +20,6 @@ const variants = {
 }
 
 export default function Home() {
-	const [display, setDisplay] = useState<string>('fetching')
-
-	useEffect(() => {
-		if (!localStorage.getItem('display')) {
-			const getDisplay = setInterval(() => {
-				if (localStorage.getItem('display') === 'hidden') {
-					setDisplay('return')
-					clearInterval(getDisplay)
-				} else {
-					setDisplay('new')
-				}
-			}, 250)
-			return () => clearInterval(getDisplay)
-		} else {
-			setDisplay('return')
-		}
-	}, [])
-
 	return (
 		<div className='h-screen w-screen'>
 			<motion.div
