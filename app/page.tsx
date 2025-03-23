613afb44f562117c734279b0f0a518b4fe7af68d
Hamaad Chughtai <Hamaad102@gmail.com>
2025-03-23 10:00:17 -0400

    animation - added lazymotion to handle hydration issues affecting animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Image from 'next/image'
-import { useState } from 'react'
-import { motion, AnimatePresence } from 'motion/react'
+import { useState, useEffect } from 'react'
+import { LazyMotion, domAnimation, m, AnimatePresence } from 'motion/react'
 
 import logo from '@/public/logo.svg'
 
@@ -19,15 +19,21 @@ interface TransitionState {
 const variants = {
 	welcome_initial: {
 		opacity: 0,
-		y: 5
+		y: 5,
+		filter: 'blur(5px)'
 	},
 	welcome_animate: {
 		opacity: 1,
 		y: 0,
+		filter: 'blur(0px)',
 		display: 'flex',
 		transition: {
 			duration: 1.2,
-			ease: [0.41, 0.03, 0.12, 0.93]
+			ease: [0.41, 0.03, 0.12, 0.93],
+			filter: {
+				duration: 1,
+				ease: [0.41, 0.03, 0.12, 0.93]
+			}
 		}
 	}
 }
@@ -44,7 +50,7 @@ const CircleTransition = ({
 	return (
 		<AnimatePresence>
 			{isActive && (
-				<motion.div
+				<m.div
 					initial={{
 						clipPath: `circle(0px at ${clickPosition.x}px ${clickPosition.y}px)`,
 						background: 'black'
@@ -78,6 +84,37 @@ export default function Home() {
 		clickPosition: { x: 0, y: 0 }
 	})
 
+	const [shouldAnimate, setShouldAnimate] = useState(false)
+
+	useEffect(() => {
+		const handlePageShow = (event: PageTransitionEvent) => {
+			if (event.persisted) {
+				setTransition({
+					active: false,
+					destination: '',
+					clickPosition: { x: 0, y: 0 }
+				})
+
+				setShouldAnimate(false)
+				setTimeout(() => setShouldAnimate(true), 50)
+			}
+		}
+
+		setTransition({
+			active: false,
+			destination: '',
+			clickPosition: { x: 0, y: 0 }
+		})
+
+		setTimeout(() => setShouldAnimate(true), 50)
+
+		window.addEventListener('pageshow', handlePageShow)
+
+		return () => {
+			window.removeEventListener('pageshow', handlePageShow)
+		}
+	}, [])
+
 	const handleLinkClick = (
 		e: React.MouseEvent<HTMLAnchorElement>,
 		href: string
@@ -94,68 +131,83 @@ export default function Home() {
 	}
 
 	return (
-		<div className='h-screen w-screen overflow-hidden'>
-			<CircleTransition
-				isActive={transition.active}
-				destination={transition.destination}
-				clickPosition={transition.clickPosition}
-			/>
-			<motion.div
-				variants={variants}
-				initial={'welcome_initial'}
-				animate={'welcome_animate'}
-				style={{
-					willChange: 'transform, opacity, filter',
-					backfaceVisibility: 'hidden',
-					WebkitBackfaceVisibility: 'hidden'
-				}}
-				className='h-screen flex-col items-center justify-center overflow-hidden bg-white'
-			>
-				<h2 className='font-extralight max-md:hidden' id='profile'>
-					I&apos;m <span className='font-semibold'>Hamaad Chughtai</span>
-					<br />a{' '}
-					<span className='font-semibold' id='title'>
-						{process.env.USER_TITLE}
-					</span>
-					<br />
-					at <span className='font-semibold'>{process.env.USER_COMPANY}</span>
-				</h2>
-				<Image src={logo} alt='HC' height={100} className='md:hidden' />
-				<h6
-					id='social'
-					className='absolute bottom-3 font-extralight max-md:left-1 max-md:self-start md:right-8 md:self-end'
-				>
-					find me on
-					<br className='hidden max-md:block' />{' '}
-					<a
-						href='https://bsky.app/profile/hamaadchughtai.com'
-						className='font-semibold'
-						onClick={(e) =>
-							handleLinkClick(e, 'https://bsky.app/profile/hamaadchughtai.com')
-						}
-					>
-						Bluesky
-					</a>
-					,{' '}
-					<a
-						href='https://github.com/Hamaad102'
-						className='font-semibold'
-						onClick={(e) => handleLinkClick(e, 'https://github.com/Hamaad102')}
-					>
-						Github
-					</a>{' '}
-					and{' '}
-					<a
-						href='https://medium.com/@hamaadchughtai'
-						className='font-semibold'
-						onClick={(e) =>
-							handleLinkClick(e, 'https://medium.com/@hamaadchughtai')
-						}
-					>
-						Medium
-					</a>
-				</h6>
-			</motion.div>
-		</div>
+		<LazyMotion features={domAnimation}>
+			<div className='h-screen w-screen overflow-hidden'>
+				<CircleTransition
+					isActive={transition.active}
+					destination={transition.destination}
+					clickPosition={transition.clickPosition}
+				/>
+				<AnimatePresence>
+					{shouldAnimate && (
+						<m.div
+							key='welcome-animation'
+							variants={variants}
+							initial='welcome_initial'
+							animate='welcome_animate'
+							style={{
+								willChange: 'transform, opacity, filter',
+								backfaceVisibility: 'hidden',
+								WebkitBackfaceVisibility: 'hidden'
+							}}
+							className='h-screen flex-col items-center justify-center overflow-hidden bg-white'
+						>
+							<h2 className='font-extralight max-md:hidden' id='profile'>
+								I&apos;m <span className='font-semibold'>Hamaad Chughtai</span>
+								<br />a{' '}
+								<span className='font-semibold' id='title'>
+									{process.env.USER_TITLE}
+								</span>
+								<br />
+								at{' '}
+								<span className='font-semibold'>
+									{process.env.USER_COMPANY}
+								</span>
+							</h2>
+							<Image src={logo} alt='HC' height={100} className='md:hidden' />
+							<h6
+								id='social'
+								className='absolute bottom-3 font-extralight max-md:left-1 max-md:self-start md:right-8 md:self-end'
+							>
+								find me on
+								<br className='hidden max-md:block' />{' '}
+								<a
+									href='https://bsky.app/profile/hamaadchughtai.com'
+									className='font-semibold'
+									onClick={(e) =>
+										handleLinkClick(
+											e,
+											'https://bsky.app/profile/hamaadchughtai.com'
+										)
+									}
+								>
+									Bluesky
+								</a>
+								,{' '}
+								<a
+									href='https://github.com/Hamaad102'
+									className='font-semibold'
+									onClick={(e) =>
+										handleLinkClick(e, 'https://github.com/Hamaad102')
+									}
+								>
+									Github
+								</a>{' '}
+								and{' '}
+								<a
+									href='https://medium.com/@hamaadchughtai'
+									className='font-semibold'
+									onClick={(e) =>
+										handleLinkClick(e, 'https://medium.com/@hamaadchughtai')
+									}
+								>
+									Medium
+								</a>
+							</h6>
+						</m.div>
+					)}
+				</AnimatePresence>
+			</div>
+		</LazyMotion>
 	)
 }
